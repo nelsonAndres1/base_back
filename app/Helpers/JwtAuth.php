1f94125af2dafe71384b3677a5539dbd523d8678
nelsonAndres1 <andrew32118@gmail.com>
2023-02-27 17:17:55 -0500

    ajustes

--- app/Helpers/JwtAuth.php
+++ app/Helpers/JwtAuth.php
@@ -250,7 +250,7 @@ class JwtAuth
         if ($nomin02) {
             foreach ($nomin02 as $key) {
                 $array = array(
-                    'nomemp' => trim(utf8_decode(utf8_encode($key->nomemp))) . ' ' . trim(utf8_decode(utf8_encode($key->segnom))) . ' ' . trim(utf8_decode(utf8_encode($key->priape))) . ' ' . trim(utf8_decode(utf8_encode($key->segape))),
+                    'nomemp' => trim(utf8_decode($key->nomemp)) . ' ' . trim(utf8_decode($key->segnom)) . ' ' . trim(utf8_decode($key->priape)) . ' ' . trim(utf8_decode($key->segape)),
                     'docemp' => $key->docemp,
                 );
                 array_push($arrayN, $array);
